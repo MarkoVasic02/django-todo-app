2b491f8962a2883e41cc8c98f56654b395da5f73
MarkoVasic02 <vasicmarko390@gmail.com>
2020-08-05 14:32:13 +0200

    Allow host

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'djyyz@f55p8*kpym5q^&*ewpygr-6qwxd--1q%6h8*gu28ctfs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['markovasic.pythonanywhere.com']
 
 
 # Application definition
